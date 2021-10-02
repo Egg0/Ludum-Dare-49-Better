3a5b12e3c8d3d655b2b2d35d3eabd3344186ec17
Lychee <marcuslr@live.com>
2021-10-01 18:56:59 -0700

    Sample shit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float velocity;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log("Initialized player man");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxisRaw("Horizontal");
+        if (movement != 0) {
+            rb.velocity = new Vector2(velocity * movement, rb.velocity.y);
+        } else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Jump");
+            rb.velocity += Vector2.up * jumpForce;
+        }
+    }
+}
