22384d1d1e55d16292178db20ee18be8b3cf5b26
jungo3 <justin.t.ngo@live.com>
2021-10-02 14:34:14 -0700

    Add egg

---
+++ Assets/Scripts/EnemyAttacks/DropEggs.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropEggs : MonoBehaviour
+{
+    public GameObject eggPrefab;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timer)
+        {
+            Drop();
+        }
+    }
+
+    void Drop()
+    {
+        Debug.Log("Dropping egg");
+        Instantiate(eggPrefab, transform.position, transform.rotation);
+    }
+}
